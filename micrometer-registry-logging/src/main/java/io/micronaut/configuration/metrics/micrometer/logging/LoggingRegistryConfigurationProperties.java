a4bb11fa97d43ff129c2e650bfd33ef3c2bbdf63
Christophe Roudet <croudet@stratacache.com>
2019-02-20 13:21:31 -0500

    Add support for LoggingMeterRegistry
    Gives to ability to dump the metrics in the logs.
    For instance for logback add the following logger in logback.xml:
    
        <logger name="io.micrometer.core.instrument.logging.LoggingMeterRegistry"
                additivity="false" level="info">
                <appender-ref ref="metricsAppender" />
        </logger>

---
+++ micrometer-registry-logging/src/main/java/io/micronaut/configuration/metrics/micrometer/logging/LoggingRegistryConfigurationProperties.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2017-2019 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.configuration.metrics.micrometer.logging;
+
+import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.context.env.Environment;
+import io.micronaut.core.naming.NameUtils;
+
+import static io.micronaut.configuration.metrics.micrometer.MeterRegistryFactory.MICRONAUT_METRICS_EXPORT;
+
+import java.util.Optional;
+import java.util.Properties;
+
+import io.micrometer.core.instrument.logging.LoggingRegistryConfig;
+
+/**
+ * {@inheritDoc}.
+ *
+ * @author Christophe Roudet
+ * @see LoggingRegistryConfig
+ */
+@ConfigurationProperties(MICRONAUT_METRICS_EXPORT)
+public class LoggingRegistryConfigurationProperties implements LoggingRegistryConfig {
+    private final Properties config;
+
+    /**
+     * Constructor for wiring a config which will use environment specific or create
+     * new.
+     *
+     * @param environment Environment
+     */
+    public LoggingRegistryConfigurationProperties(Environment environment) {
+        Optional<Properties> cfg = environment.getProperty(MICRONAUT_METRICS_EXPORT, Properties.class);
+        this.config = cfg.orElseGet(Properties::new);
+    }
+
+    /**
+     * Method to get config param. Will hyphenate the properties.
+     *
+     * @param k key
+     * @return String value of property
+     */
+    @Override
+    public String get(String k) {
+        return config.getProperty(NameUtils.hyphenate(k));
+    }
+}
---
+++ micrometer-registry-logging/src/test/groovy/io/micronaut/configuration/metrics/micrometer/logging/LoggingMeterRegistryFactorySpec.groovy
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2017-2019 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.configuration.metrics.micrometer.logging
+
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry
+import io.micrometer.core.instrument.logging.LoggingMeterRegistry
+import io.micronaut.configuration.metrics.micrometer.MeterRegistryCreationListener
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.env.Environment
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.time.Duration
+
+import static io.micronaut.configuration.metrics.micrometer.MeterRegistryFactory.MICRONAUT_METRICS_ENABLED
+import static io.micronaut.configuration.metrics.micrometer.logging.LoggingMeterRegistryFactory.LOGGING_CONFIG
+import static io.micronaut.configuration.metrics.micrometer.logging.LoggingMeterRegistryFactory.LOGGING_ENABLED
+
+class LoggingMeterRegistryFactorySpec extends Specification {
+
+    def "wireup the bean manually"() {
+        given:
+        Environment mockEnvironment = Stub()
+        mockEnvironment.getProperty(_, _) >> Optional.empty()
+
+        when:
+        LoggingMeterRegistryFactory factory = new LoggingMeterRegistryFactory(new LoggingRegistryConfigurationProperties(mockEnvironment))
+
+        then:
+        factory.loggingMeterRegistry()
+    }
+
+    void "verify LoggingMeterRegistry is created by default when this configuration used"() {
+        when:
+        ApplicationContext context = ApplicationContext.run()
+
+        then:
+        context.getBeansOfType(MeterRegistry).size() == 2
+        context.getBeansOfType(MeterRegistry)*.class*.simpleName.containsAll(['CompositeMeterRegistry', 'LoggingMeterRegistry'])
+
+        cleanup:
+        context.stop()
+    }
+
+    void "verify CompositeMeterRegistry created by default"() {
+        when:
+        ApplicationContext context = ApplicationContext.run()
+        CompositeMeterRegistry compositeRegistry = context.getBean(CompositeMeterRegistry)
+
+        then:
+        compositeRegistry
+        context.getBean(MeterRegistryCreationListener)
+        context.getBean(LoggingMeterRegistry)
+        compositeRegistry.registries.size() == 1
+        compositeRegistry.registries*.class.containsAll([LoggingMeterRegistry])
+
+        cleanup:
+        context.stop()
+    }
+
+    @Unroll
+    void "verify LoggingMeterRegistry bean exists = #result when config #cfg = #setting"() {
+        when:
+        ApplicationContext context = ApplicationContext.run([(cfg): setting])
+
+        then:
+        context.findBean(LoggingMeterRegistry).isPresent() == result
+
+        where:
+        cfg                       | setting | result
+        MICRONAUT_METRICS_ENABLED | false   | false
+        MICRONAUT_METRICS_ENABLED | true    | true
+        LOGGING_ENABLED            | true    | true
+        LOGGING_ENABLED            | false   | false
+    }
+
+
+    void "verify LoggingMeterRegistry bean exists changed step"() {
+        when:
+        ApplicationContext context = ApplicationContext.run([
+                (LOGGING_ENABLED)           : true,
+                (LOGGING_CONFIG + ".step")  : "PT2M",
+        ])
+        Optional<LoggingMeterRegistry> loggingMeterRegistry = context.findBean(LoggingMeterRegistry)
+
+        then:
+        loggingMeterRegistry.isPresent()
+        loggingMeterRegistry.get().config.enabled()
+        loggingMeterRegistry.get().config.step() == Duration.ofMinutes(2)
+
+        cleanup:
+        context.stop()
+    }
+}
