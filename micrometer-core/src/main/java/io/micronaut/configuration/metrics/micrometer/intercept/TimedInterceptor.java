ffbf0e9cc01fcac1ea1182a3c162d32cc2afe44b
croudet <croudet@gmail.com>
2020-06-12 13:22:58 -0400

    Issue #70 - Detect kotlin coroutine calls.

--- micrometer-core/src/main/java/io/micronaut/configuration/metrics/micrometer/intercept/TimedInterceptor.java
+++ micrometer-core/src/main/java/io/micronaut/configuration/metrics/micrometer/intercept/TimedInterceptor.java
@@ -24,17 +24,19 @@ import io.micronaut.configuration.metrics.annotation.RequiresMetrics;
 import io.micronaut.core.annotation.AnnotationMetadata;
 import io.micronaut.core.annotation.TypeHint;
 import io.micronaut.core.async.publisher.Publishers;
+import io.micronaut.core.util.KotlinUtils;
 import io.micronaut.core.util.StringUtils;
 import io.reactivex.Flowable;
 import io.reactivex.Single;
-import io.reactivex.functions.Action;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Singleton;
+
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.BiConsumer;
+import java.util.function.Supplier;
 
 /**
  * Implements support for {@link io.micrometer.core.annotation.Timed} as AOP advice.
@@ -75,86 +77,116 @@ public class TimedInterceptor implements MethodInterceptor<Object, Object> {
 
     @Override
     public Object intercept(MethodInvocationContext<Object, Object> context) {
-        final Class<Object> javaReturnType = context.getReturnType().getType();
         final AnnotationMetadata metadata = context.getAnnotationMetadata();
         final String metricName = metadata.getValue(Timed.class, String.class).orElse(DEFAULT_METRIC_NAME);
         if (StringUtils.isNotEmpty(metricName)) {
+            // check for kotlin continuation
+            if (context.getExecutableMethod().isSuspend()) {
+                return handleKotlinCoroutine(context, metadata, metricName);
+            }
+            final Class<Object> javaReturnType = context.getReturnType().getType();
             if (Publishers.isConvertibleToPublisher(javaReturnType)) {
-
                 // handle publisher
+                return handlePublisher(context, metadata, metricName, javaReturnType);
+
+            }
+            // blocking case
+            final Timer.Sample sample = Timer.start(meterRegistry);
+            String exceptionClass = "none";
+            boolean stop = true;
+            try {
                 final Object result = context.proceed();
-                if (result == null) {
-                    return result;
+                if (result instanceof CompletionStage) {
+                    stop = false;
+                    CompletionStage<?> cs = (CompletionStage<?>) result;
+                    return cs.whenComplete((o, throwable) -> stopTimed(
+                            metricName,
+                            sample,
+                            throwable == null ? "none" : throwable.getClass().getSimpleName(),
+                            metadata));
                 } else {
-                    AtomicReference<Timer.Sample> sample = new AtomicReference<>();
-                    AtomicReference<String> exceptionClass = new AtomicReference<>("none");
-                    if (Publishers.isSingle(result.getClass())) {
-                        Single<?> single = Publishers.convertPublisher(result, Single.class);
-                        single = single.doOnSubscribe(d -> sample.set(Timer.start(meterRegistry)))
-                                       .doOnError(throwable -> stopTimed(
-                                               metricName,
-                                               sample.get(),
-                                               throwable.getClass().getSimpleName(),
-                                               metadata
-                                       ))
-                                       .doOnSuccess(o -> stopTimed(
-                                               metricName,
-                                               sample.get(),
-                                               "none",
-                                               metadata
-                                       ));
-
-                        return Publishers.convertPublisher(single, javaReturnType);
-                    } else {
-                        Flowable<?> flowable = Publishers.convertPublisher(result, Flowable.class);
-                        flowable = flowable.doOnRequest(n -> sample.set(Timer.start(meterRegistry)))
-                                .doOnError(throwable -> exceptionClass.set(throwable.getClass().getSimpleName()))
-                                .doOnComplete((Action) () -> {
-                                    final Timer.Sample s = sample.get();
-                                    if (s != null) {
-                                        stopTimed(
-                                                metricName,
-                                                s,
-                                                exceptionClass.get(),
-                                                metadata
-                                        );
-                                    }
-                                });
-                        return Publishers.convertPublisher(flowable, javaReturnType);
-                    }
+                    return result;
                 }
-
-
-            } else {
-                // blocking case
-                Timer.Sample sample = Timer.start(meterRegistry);
-                String exceptionClass = "none";
-                boolean stop = true;
-                try {
-                    final Object result = context.proceed();
-                    if (result instanceof CompletionStage) {
-                        stop = false;
-                        CompletionStage<?> cs = (CompletionStage<?>) result;
-                        return cs.whenComplete((BiConsumer<Object, Throwable>) (o, throwable) -> stopTimed(
-                                metricName,
-                                sample,
-                                throwable != null ? throwable.getClass().getSimpleName() : "none",
-                                metadata));
-                    } else {
-                        return result;
-                    }
-                } catch (Exception e) {
-                    exceptionClass = e.getClass().getSimpleName();
-                    throw e;
-                } finally {
-                    if (stop) {
-
-                        stopTimed(metricName, sample, exceptionClass, metadata);
-                    }
+            } catch (Exception e) {
+                exceptionClass = e.getClass().getSimpleName();
+                throw e;
+            } finally {
+                if (stop) {
+                    stopTimed(metricName, sample, exceptionClass, metadata);
                 }
             }
         }
         return context.proceed();
+    }
+
+    private Object handlePublisher(MethodInvocationContext<Object, Object> context, final AnnotationMetadata metadata, final String metricName, final Class<Object> javaReturnType) {
+        final Object result = context.proceed();
+        if (result == null) {
+            return result;
+        } else {
+            AtomicReference<Timer.Sample> sample = new AtomicReference<>();
+            if (Publishers.isSingle(result.getClass())) {
+                Single<?> single = Publishers.convertPublisher(result, Single.class);
+                single = single.doOnSubscribe(d -> sample.set(Timer.start(meterRegistry)))
+                        .doOnError(throwable -> stopTimed(
+                                metricName,
+                                sample.get(),
+                                throwable.getClass().getSimpleName(),
+                                metadata))
+                        .doOnSuccess(o -> stopTimed(
+                                metricName,
+                                sample.get(),
+                                "none",
+                                metadata));
+
+                return Publishers.convertPublisher(single, javaReturnType);
+            } else {
+                AtomicReference<String> exceptionClass = new AtomicReference<>("none");
+                Flowable<?> flowable = Publishers.convertPublisher(result, Flowable.class);
+                flowable = flowable.doOnRequest(n -> sample.set(Timer.start(meterRegistry)))
+                        .doOnError(throwable -> exceptionClass.set(throwable.getClass().getSimpleName()))
+                        .doOnComplete(() -> {
+                            final Timer.Sample s = sample.get();
+                            if (s != null) {
+                                stopTimed(
+                                        metricName,
+                                        s,
+                                        exceptionClass.get(),
+                                        metadata);
+                            }
+                        });
+                return Publishers.convertPublisher(flowable, javaReturnType);
+            }
+        }
+    }
+
+    private Object handleKotlinCoroutine(MethodInvocationContext<Object, Object> context, final AnnotationMetadata metadata, final String metricName) {
+        final Timer.Sample sample = Timer.start(meterRegistry);
+        final Object result = context.proceed();
+        // is it suspended?
+        if (result == null || !KotlinUtils.isKotlinCoroutineSuspended(result)) {
+            // not suspended
+            stopTimed(
+                    metricName,
+                    sample,
+                    result instanceof Exception ? result.getClass().getName() : "none",
+                    metadata);
+        } else {
+            // last argument
+            final Object[] arguments = context.getParameterValues();
+            final Object lastArgument = arguments[arguments.length - 1];
+            if (lastArgument instanceof Supplier) {
+                final CompletableFuture<?> future = ((Supplier<CompletableFuture<?>>) lastArgument).get();
+                future.whenComplete((lastResult, exception) ->
+                    stopTimed(
+                            metricName,
+                            sample,
+                            exception == null ? "none" : exception.getClass().getSimpleName(),
+                            metadata)
+                );
+            }
+        }
+        return result;
     }
 
     private void stopTimed(String metricName, Timer.Sample sample, String exceptionClass, AnnotationMetadata metadata) {
@@ -177,4 +209,5 @@ public class TimedInterceptor implements MethodInterceptor<Object, Object> {
             }
         }
     }
+
 }
