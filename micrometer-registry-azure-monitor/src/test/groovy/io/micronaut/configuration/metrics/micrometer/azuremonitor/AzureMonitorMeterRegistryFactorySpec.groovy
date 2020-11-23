5cc4aaf042b0b53b9c748f8f3ccc9be776f5414a
graemerocher <graeme.rocher@gmail.com>
2020-11-23 12:41:34 +0100

    Fix tests

--- micrometer-registry-azure-monitor/src/test/groovy/io/micronaut/configuration/metrics/micrometer/azuremonitor/AzureMonitorMeterRegistryFactorySpec.groovy
+++ micrometer-registry-azure-monitor/src/test/groovy/io/micronaut/configuration/metrics/micrometer/azuremonitor/AzureMonitorMeterRegistryFactorySpec.groovy
@@ -15,14 +15,18 @@
  */
 package io.micronaut.configuration.metrics.micrometer.azuremonitor
 
+import io.micrometer.azuremonitor.AzureMonitorConfig
 import io.micrometer.azuremonitor.AzureMonitorMeterRegistry
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.composite.CompositeMeterRegistry
+import io.micrometer.core.instrument.step.StepMeterRegistry
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.exceptions.BeanInstantiationException
+import io.micronaut.core.reflect.ReflectionUtils
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import java.lang.reflect.Field
 import java.time.Duration
 
 import static io.micronaut.configuration.metrics.micrometer.MeterRegistryFactory.MICRONAUT_METRICS_ENABLED
@@ -99,10 +103,16 @@ class AzureMonitorMeterRegistryFactorySpec extends Specification {
 
         then: "default properties are used"
         azureMonitorMeterRegistry.isPresent()
-        azureMonitorMeterRegistry.get().config.enabled()
-        azureMonitorMeterRegistry.get().config.numThreads() == 2
-        azureMonitorMeterRegistry.get().config.batchSize() == 10000
-        azureMonitorMeterRegistry.get().config.step() == Duration.ofMinutes(1)
+
+        and:
+        def meterRegistry = azureMonitorMeterRegistry.get()
+        def field = ReflectionUtils.getRequiredField(StepMeterRegistry, "config")
+        field.setAccessible(true)
+        AzureMonitorConfig config = field.get(meterRegistry)
+
+        config.enabled()
+        config.batchSize() == 10000
+        config.step() == Duration.ofMinutes(1)
 
         cleanup:
         context.stop()
@@ -121,10 +131,14 @@ class AzureMonitorMeterRegistryFactorySpec extends Specification {
 
         then:
         azureMonitorMeterRegistry.isPresent()
-        azureMonitorMeterRegistry.get().config.enabled()
-        azureMonitorMeterRegistry.get().config.numThreads() == 77
-        azureMonitorMeterRegistry.get().config.instrumentationKey() == MOCK_AZURE_INSTRUMENTATION_KEY
-        azureMonitorMeterRegistry.get().config.step() == Duration.ofMinutes(2)
+
+        and:
+        def meterRegistry = azureMonitorMeterRegistry.get()
+        def field = ReflectionUtils.getRequiredField(StepMeterRegistry, "config")
+        field.setAccessible(true)
+        AzureMonitorConfig config = field.get(meterRegistry)
+        config.instrumentationKey() == MOCK_AZURE_INSTRUMENTATION_KEY
+        config.step() == Duration.ofMinutes(2)
 
         cleanup:
         context.stop()
