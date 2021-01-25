997abe0999c281631e3b881243fd6edea45b8608
Michael Parlee <michael.parlee@dexcom.com>
2021-01-25 14:20:48 -0800

    adds support for new relic's telemetry sdk based micrometer registry

---
+++ micrometer-registry-new-relic-telemetry/src/main/java/io/micronaut/configuration/metrics/micrometer/newrelic/NewRelicRegistryFactory.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2017-2019 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.configuration.metrics.micrometer.newrelic;
+import com.newrelic.telemetry.Attributes;
+import com.newrelic.telemetry.micrometer.NewRelicRegistry;
+import com.newrelic.telemetry.micrometer.NewRelicRegistryConfig;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.util.NamedThreadFactory;
+import io.micronaut.configuration.metrics.micrometer.ExportConfigurationProperties;
+import io.micronaut.context.annotation.Factory;
+
+import javax.inject.Singleton;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Properties;
+
+import static io.micronaut.configuration.metrics.micrometer.MeterRegistryFactory.MICRONAUT_METRICS_EXPORT;
+
+/**
+ * The NewRelicRegistryFactory that will configure and create a new relic registry using the
+ * New Relic provided micrometer registr.
+ *
+ * @author mparlee
+ */
+@Factory
+public class NewRelicRegistryFactory {
+
+    public static final String NEWRELIC_CONFIG = MICRONAUT_METRICS_EXPORT + ".newrelic";
+    public static final String NEWRELIC_ENABLED = NEWRELIC_CONFIG + ".enabled";
+
+    /**
+     * Create a NewRelicRegistry bean if global metrics are enables
+     * and the newrelic is enabled.  Will be true by default when this
+     * configuration is included in project.
+     *
+     * @param config The the new relic config
+     * @return A NewRelicRegistry
+     */
+    @Singleton
+    NewRelicRegistry newRelicRegistry(NewRelicRegistryConfig config) throws UnknownHostException {
+        NewRelicRegistry newRelicRegistry =
+                NewRelicRegistry.builder(config)
+                        .commonAttributes(
+                                new Attributes()
+                                        .put("host", InetAddress.getLocalHost().getHostName()))
+                        .build();
+        newRelicRegistry.config().meterFilter(MeterFilter.ignoreTags("plz_ignore_me"));
+        newRelicRegistry.config().meterFilter(MeterFilter.denyNameStartsWith("jvm.threads"));
+        newRelicRegistry.start(new NamedThreadFactory("newrelic.micrometer.registry"));
+        return newRelicRegistry;
+    }
+
+    /**
+     * The new relic config bean.
+     * @param exportConfigurationProperties The properties
+     * @return The new relic bean
+     */
+    @Singleton
+    NewRelicMicronautConfig newRelicRegistryConfig(ExportConfigurationProperties exportConfigurationProperties) {
+        Properties exportConfig = exportConfigurationProperties.getExport();
+        return exportConfig::getProperty;
+    }
+
+}
