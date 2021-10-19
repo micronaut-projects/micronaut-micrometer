d669515b42ec917fab3ba6120d17448cf138921f
Marcin Sucharski <marcin.sucharski.github@gmail.com>
2021-10-19 17:28:22 +0200

    Fix Netty's queue metrics (#302)
    Due to bean type and names inconsistency between the DefaultEventLoopGroupFactory in the micronaut-core
    and Instrumented{Epoll,KQueue,Nio}EventLoopGroupFactory in the micrometer-core, instrumented beans
    were not injected. As a result, metrics were not working.

---
+++ micrometer-core/src/test/groovy/io/micronaut/configuration/metrics/binder/netty/MicronautNettyQueuesMetricsBinderSpec.groovy
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2017-2021 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.configuration.metrics.binder.netty
+
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Tags
+import io.micrometer.core.instrument.Timer
+import io.micrometer.core.instrument.search.RequiredSearch
+import io.micronaut.context.ApplicationContext
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.runtime.server.EmbeddedServer
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static io.micronaut.configuration.metrics.binder.netty.NettyMetrics.COUNT
+import static io.micronaut.configuration.metrics.binder.netty.NettyMetrics.ELEMENT
+import static io.micronaut.configuration.metrics.binder.netty.NettyMetrics.EXECUTION_TIME
+import static io.micronaut.configuration.metrics.binder.netty.NettyMetrics.GLOBAL
+import static io.micronaut.configuration.metrics.binder.netty.NettyMetrics.GROUP
+import static io.micronaut.configuration.metrics.binder.netty.NettyMetrics.NETTY
+import static io.micronaut.configuration.metrics.binder.netty.NettyMetrics.PARENT
+import static io.micronaut.configuration.metrics.binder.netty.NettyMetrics.QUEUE
+import static io.micronaut.configuration.metrics.binder.netty.NettyMetrics.WAIT_TIME
+import static io.micronaut.configuration.metrics.binder.netty.NettyMetrics.WORKER
+import static io.micronaut.configuration.metrics.binder.netty.NettyMetrics.dot
+import static io.micronaut.configuration.metrics.micrometer.MeterRegistryFactory.MICRONAUT_METRICS_BINDERS
+import static io.micronaut.configuration.metrics.micrometer.MeterRegistryFactory.MICRONAUT_METRICS_ENABLED
+
+class MicronautNettyQueuesMetricsBinderSpec extends Specification {
+
+    private static def eventLoopGroupFactoryInstrumentedClasses = [
+            InstrumentedNioEventLoopGroupFactory,
+            InstrumentedEpollEventLoopGroupFactory,
+            InstrumentedKQueueEventLoopGroupFactory
+    ]
+
+    @Unroll
+    def "test getting the beans #cfg #setting"() {
+        when:
+        ApplicationContext context = ApplicationContext.run([(cfg): setting])
+
+        then:
+        eventLoopGroupFactoryInstrumentedClasses
+                .collect { context.findBean(it).isPresent() }
+                .any() == result
+
+        cleanup:
+        context.close()
+
+        where:
+        cfg                                                 | setting | result
+        MICRONAUT_METRICS_ENABLED                           | true    | false
+        MICRONAUT_METRICS_ENABLED                           | false   | false
+        MICRONAUT_METRICS_BINDERS + ".netty.queues.enabled" | true    | true
+        MICRONAUT_METRICS_BINDERS + ".netty.queues.enabled" | false   | false
+    }
+
+    def "test queue metrics are present"() {
+        when:
+        ApplicationContext context = ApplicationContext.run(
+                [MICRONAUT_METRICS_ENABLED                            : true,
+                 (MICRONAUT_METRICS_BINDERS + ".netty.queues.enabled"): true]
+        )
+        def server = context.getBean(EmbeddedServer)
+        server.start()
+
+        then:
+        eventLoopGroupFactoryInstrumentedClasses
+                .collect { context.findBean(it).isPresent() }
+                .any()
+
+        when:
+        MeterRegistry registry = context.getBean(MeterRegistry)
+        RequiredSearch search = registry.get(dot(NETTY, QUEUE, GLOBAL, WAIT_TIME))
+        search.tags(Tags.of(GROUP, PARENT))
+        Timer globalParentWaitTimer = search.timer()
+
+        search = registry.get(dot(NETTY, QUEUE, GLOBAL, EXECUTION_TIME))
+        search.tags(Tags.of(GROUP, PARENT))
+        Timer globalParentExecutionTime = search.timer()
+
+        search = registry.get(dot(NETTY, QUEUE, GLOBAL, ELEMENT, COUNT))
+        search.tags(Tags.of(GROUP, PARENT))
+        Counter globalParentTaskCounter = search.counter()
+
+        search = registry.get(dot(NETTY, QUEUE, GLOBAL, WAIT_TIME))
+        search.tags(Tags.of(GROUP, WORKER))
+        Timer globalWorkerWaitTimer = search.timer()
+
+        search = registry.get(dot(NETTY, QUEUE, GLOBAL, EXECUTION_TIME))
+        search.tags(Tags.of(GROUP, WORKER))
+        Timer globalWorkerExecutionTimer = search.timer()
+
+        search = registry.get(dot(NETTY, QUEUE, GLOBAL, ELEMENT, COUNT))
+        search.tags(Tags.of(GROUP, WORKER))
+        Counter globalWorkerTaskCounter = search.counter()
+
+        then:
+        globalParentWaitTimer
+        globalParentExecutionTime
+        globalParentTaskCounter
+
+        globalWorkerWaitTimer
+        globalWorkerWaitTimer.count() == 0
+        globalWorkerExecutionTimer
+        globalWorkerExecutionTimer.count() == 0
+        globalWorkerTaskCounter
+        globalWorkerTaskCounter.count() == 0
+
+        when:
+        DummyClient client = context.getBean(DummyClient)
+
+        then:
+        client.test() == 'root'
+        client.test() == 'root'
+        client.test() == 'root'
+        client.test() == 'root'
+        client.test() == 'root'
+        client.test() == 'root'
+        globalParentWaitTimer.count() > 0
+        globalParentExecutionTime.count() > 0
+        globalWorkerWaitTimer.count() > 0
+        globalWorkerExecutionTimer.count() > 0
+        globalParentTaskCounter.count() > 0
+        globalWorkerTaskCounter.count() > 0
+
+        cleanup:
+        context.close()
+    }
+
+    @Client('/nettyQueuesMetricsTest')
+    private static interface DummyClient {
+        @Get
+        String test()
+    }
+
+    @Controller('/nettyQueuesMetricsTest')
+    private static class DummyController {
+        @Get
+        String root() {
+            return "root"
+        }
+    }
+}
