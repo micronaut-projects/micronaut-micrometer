e7c486322a493edd0d02c8c0a578167fe19fac0f
radovanradic <radicr@gmail.com>
2026-02-05 15:36:21 +0100

    Update proxy resolver to run as the last one

--- micrometer-observation-datasource/src/main/java/io/micronaut/micrometer/observation/datasource/ProxyDataSourceResolver.java
+++ micrometer-observation-datasource/src/main/java/io/micronaut/micrometer/observation/datasource/ProxyDataSourceResolver.java
@@ -15,13 +15,12 @@
  */
 package io.micronaut.micrometer.observation.datasource;
 
-import io.micronaut.context.annotation.Primary;
+import io.micronaut.core.order.Ordered;
 import io.micronaut.jdbc.DataSourceResolver;
 import jakarta.inject.Singleton;
 import net.ttddyy.dsproxy.support.ProxyDataSource;
 
 import javax.sql.DataSource;
-import java.util.List;
 
 /**
  * A custom implementation of {@link DataSourceResolver} that resolves the underlying target data source
@@ -34,25 +33,19 @@ import java.util.List;
  * @since 5.10
  */
 @Singleton
-@Primary
-final class ProxyDataSourceResolver implements DataSourceResolver {
-
-    private final List<DataSourceResolver> dataSourceResolvers;
-
-    ProxyDataSourceResolver(List<DataSourceResolver> dataSourceResolvers) {
-        this.dataSourceResolvers = dataSourceResolvers;
-    }
+final class ProxyDataSourceResolver implements DataSourceResolver, Ordered {
 
     @Override
     public DataSource resolve(DataSource dataSource) {
-        // The list of resolvers can be empty
-        for (DataSourceResolver dataSourceResolver : dataSourceResolvers) {
-            dataSource = dataSourceResolver.resolve(dataSource);
-        }
         // Unwrap ProxyDataSource if needed
         if (dataSource instanceof ProxyDataSource proxyDataSource) {
             dataSource = proxyDataSource.getDataSource();
         }
         return dataSource;
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
     }
 }
